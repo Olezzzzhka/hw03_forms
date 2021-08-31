98345c94bbbf87b11984e1a81c5ce7a5574a8c66
Oleg <oleezzzzhka@yandex.ru>
2021-08-31 22:12:01 +0300

    Проект 4 спринта

---
+++ yatube/posts/views.py
@@ -0,0 +1,106 @@
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Group, User
+from .forms import PostForm
+import datetime as dt
+
+
+def index(request):
+    post_list = Post.objects.all().order_by('-pub_date')
+
+    # Показывать по 10 записей на странице.
+    paginator = Paginator(post_list, 10) 
+
+    # Из URL извлекаем номер запрошенной страницы - это значение параметра page
+    page_number = request.GET.get('page')
+
+    # Получаем набор записей для страницы с запрошенным номером
+    page_obj = paginator.get_page(page_number)
+    # Отдаем в словаре контекста
+    context = {
+        'post_list': post_list,
+        'page_obj': page_obj,
+    }
+    return render(request, 'index.html', context) 
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+
+    post_list = group.posts.all().order_by('-pub_date')
+
+    paginator = Paginator(post_list, 10)
+
+    page_number = request.GET.get('page')
+
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        "group": group,
+        "post_list": post_list,
+        "page_obj": page_obj,
+        }
+
+    return render(
+        request, "posts/group_list.html", context
+        )
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    posts = author.posts.all().order_by('-pub_date')
+    posts_count = posts.count()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'author': author,
+        'posts': posts,
+        'page_obj': page_obj,
+        'posts_count': posts_count,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    # Здесь код запроса к модели и создание словаря контекста
+    post = get_object_or_404(Post, pk=post_id)
+    author_posts_count = Post.objects.filter(author_id=post.author_id).count()
+    context = {
+        'post': post,
+        'post_id': post_id,
+        'author_posts_count': author_posts_count,
+    }
+    return render(request, 'posts/post_detail.html', context)
+
+
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST or None)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('posts:profile', username=request.user.username)
+    else:
+        form = PostForm()
+    return render(request, 'posts/create_post.html', {'form': form})
+
+
+@login_required
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if post.author != request.user:
+        return redirect('posts:post_detail', post_id=post.pk)
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('posts:post_detail', post_id=post.pk)
+    context = {
+        'form': form,
+        'post': post,
+        'is_edit': True,
+    }
+
+    return render(request, 'posts/create_post.html', context)
---
+++ yatube/users/urls.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.views import LogoutView, LoginView, PasswordChangeView
+from django.urls import path
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path(
+        'login/',
+        LoginView.as_view(template_name="registration/login.html"),
+        name='login'
+    ),
+    path(
+        'logout/',
+        LogoutView.as_view(template_name="registration/logged_out.html"),
+        name='logout'
+    ),
+    path(
+        'password_change/',
+        PasswordChangeView.as_view(template_name="registration/password_change_form.html"),
+        name='password_change'
+    ),
+    path('signup/', views.SignUp.as_view(), name='signup'),
+]
